f0c703d1fe9af48e8b68df616e6927716b09fa4a
Adrien Rougny <adrienrougny@gmail.com>
2017-05-12 11:38:10 +0900

    import bugs + write_SBGNML

--- csbgnpy/pd/utils.py
+++ csbgnpy/pd/utils.py
@@ -60,7 +60,7 @@ def read_SBGNML(*filenames):
                 entities.add(entity)
         for glyph in sbgnmap.get_glyph(): # making processes
             if glyph.get_class().name in [attribute.name for attribute in list(csbgnpy.pd.ProcessClazz)]:
-                proc = utils._make_process_from_glyph(glyph, sbgnmap, entities, compartments)
+                proc = csbgnpy.pd.utils._make_process_from_glyph(glyph, sbgnmap, entities, compartments)
                 processes.add(proc)
         for glyph in sbgnmap.get_glyph(): # making logical operator nodes
             if glyph.get_class().name in [attribute.name for attribute in list(csbgnpy.pd.LogicalOperatorClazz)]:
@@ -179,7 +179,7 @@ def _make_logical_operator_node_from_glyph(glyph, sbgnmap, entities, compartment
                     op.add_child(existent_source)
                 else:
                     op.add_child(source)
-        return op
+    return op
 
 """Still have to take into account stoech !"""
 def _make_process_from_glyph(glyph, sbgnmap, entities, compartments):
@@ -219,3 +219,213 @@ def _make_modulation_from_arc(arc, sbgnmap, entities, compartments, logical_oper
     clazz = csbgnpy.pd.ModulationClazz[arc.get_class().name]
     modulation = csbgnpy.pd.Modulation(clazz, existent_source, existent_target)
     return modulation
+
+def _make_glyph_from_entity(entity):
+    g = libsbgn.glyph()
+    g.set_class(libsbgn.GlyphClass[entity.clazz.name])
+    g.set_id(entity.id)
+    bbox = libsbgn.bbox(0, 0, 0, 0)
+    g.set_bbox(bbox)
+    label = libsbgn.label()
+    if entity.has_label():
+        label.set_text(entity.label)
+    else:
+        label.set_text("")
+    g.set_label(label)
+    for subentity in entity.components:
+        gc = csbgnpy.pd.utils._make_glyph_from_subentity(subentity)
+        g.add_glyph(gc)
+    for i, sv in enumerate(entity.svs):
+        gsv = libsbgn.glyph()
+        gsv.set_id(entity.id + "_sv_{0}".format(i))
+        gsv.set_class(libsbgn.GlyphClass["STATE_VARIABLE"])
+        gsv.set_state(libsbgn.stateType(sv.variable, sv.value))
+        bbox = libsbgn.bbox(0, 0, 0, 0)
+        gsv.set_bbox(bbox)
+        g.add_glyph(gsv)
+    for i, ui in enumerate(entity.uis):
+        gui = libsbgn.glyph()
+        gui.set_id(entity.id + "_ui_{0}".format(i))
+        gui.set_class(libsbgn.GlyphClass["UNIT_OF_INFORMATION"])
+        label = libsbgn.label()
+        if ui.prefix is not None:
+            label.set_text(ui.prefix() + ':' + ui.label)
+        else:
+            label.set_text(ui.label)
+        gui.set_label(label)
+        bbox = libsbgn.bbox(0, 0, 0, 0)
+        g.add_glyph(gui)
+    return g
+
+def _make_glyph_from_logical_operator_node(op):
+    g = libsbgn.glyph()
+    g.set_class(libsbgn.GlyphClass[op.clazz.name])
+    g.set_id(op.id)
+    bbox = libsbgn.bbox(0, 0, 0, 0)
+    g.set_bbox(bbox)
+    return g
+
+def _make_glyph_from_subentity(subentity):
+    g = libsbgn.glyph()
+    g.set_class(libsbgn.GlyphClass[subentity.clazz.name[4:]]) #remove the SUB_ from the class name
+    g.set_id(subentity.id)
+    bbox = libsbgn.bbox(0, 0, 0, 0)
+    g.set_bbox(bbox)
+    label = libsbgn.label()
+    label.set_text(subentity.label)
+    g.set_label(label)
+    for subsubentity in subentity.components:
+        gc = utils._make_glyph_from_subentity(subsubentity)
+        g.add_glyph(gc)
+    for i, sv in enumerate(subentity.svs):
+        gsv = libsbgn.glyph()
+        gsv.set_id(subentity.id + "_sv_{0}".format(i))
+        gsv.set_class(libsbgn.GlyphClass["STATE_VARIABLE"])
+        gsv.set_state(libsbgn.stateType(sv.variable, sv.value))
+        bbox = libsbgn.bbox(0, 0, 0, 0)
+        gsv.set_bbox(bbox)
+        g.add_glyph(gsv)
+    for i, ui in enumerate(subentity.uis):
+        gui = libsbgn.glyph()
+        gui.set_id(subentity.id + "_ui_{0}".format(i))
+        gui.set_class(libsbgn.GlyphClass["UNIT_OF_INFORMATION"])
+        label = libsbgn.label()
+        if ui.prefix is not None:
+            label.set_text(ui.prefix() + ':' + ui.label)
+        else:
+            label.set_text(ui.label)
+        gui.set_label(label)
+        bbox = libsbgn.bbox(0, 0, 0, 0)
+        gui.set_bbox(bbox)
+        g.add_glyph(gui)
+    return g
+
+
+def _make_glyph_from_process(process):
+    p = libsbgn.glyph()
+    p.set_class(libsbgn.GlyphClass[process.clazz.name])
+    p.set_id(process.id)
+    bbox = libsbgn.bbox(0, 0, 0, 0) 
+    p.set_bbox(bbox)
+    # port1 = libsbgn.port()
+    # port1.set_id("{0}.1".format(p.get_id()))
+    # port1.set_y(bbox.get_y() + bbox.get_h() / 2)
+    # port1.set_x(bbox.get_x())
+    # port2 = libsbgn.port()
+    # port2.set_id("{0}.2".format(p.get_id()))
+    # port2.set_y(bbox.get_y() + bbox.get_h() / 2)
+    # port2.set_x(bbox.get_x() + bbox.get_w())
+    # p.add_port(port1)
+    # p.add_port(port2)
+    return p
+
+def _make_arcs_from_process(process, dids):
+    arcs = []
+    for reactant in process.reactants:
+        arc = libsbgn.arc()
+        start = libsbgn.startType(0, 0)
+        end = libsbgn.endType(0, 0)
+        arc.set_source(dids[reactant])
+        # arc.set_target("{0}.1".format(process.getId()))
+        arc.set_target(dids[process])
+        arc.set_id("arc_{0}_{1}".format(dids[reactant], dids[process]))
+        arc.set_start(start)
+        arc.set_end(end)
+        arc.set_class(libsbgn.ArcClass.CONSUMPTION)
+        arcs.append(arc)
+    for product in process.products:
+        arc = libsbgn.arc()
+        start = libsbgn.startType(0, 0)
+        end = libsbgn.endType(0, 0)
+        # arc.set_source("{0}.2".format(process.getId()))
+        arc.set_source(dids[process])
+        arc.set_target(dids[product])
+        arc.set_id("arc_{0}_{1}".format(dids[product], dids[process]))
+        arc.set_start(start)
+        arc.set_end(end)
+        arc.set_class(libsbgn.ArcClass.PRODUCTION)
+        arcs.append(arc)
+    return arcs
+
+def _make_arc_from_modulation(modulation, dids):
+    arc = libsbgn.arc()
+    start = libsbgn.startType(0, 0)
+    end = libsbgn.endType(0, 0)
+    arc.set_source(dids[modulation.source])
+    arc.set_target(dids[modulation.target])
+    arc.set_id("arc_{0}_{1}".format(dids[modulation.source], dids[modulation.target]))
+    arc.set_start(start)
+    arc.set_end(end)
+    arc.set_class(libsbgn.ArcClass[modulation.clazz.name])
+    return arc
+
+def _make_arcs_from_logical_operator_node(op, dids):
+    arcs = set()
+    for child in op.children:
+        arc = libsbgn.arc()
+        start = libsbgn.startType(0, 0)
+        end = libsbgn.endType(0, 0)
+        arc.set_source(dids[child])
+        arc.set_target(dids[op])
+        arc.set_id("arc_{0}_{1}".format(dids[child], dids[op]))
+        arc.set_start(start)
+        arc.set_end(end)
+        arc.set_class(libsbgn.ArcClass["LOGIC_ARC"])
+        arcs.add(arc)
+    return arcs
+
+def _renew_ids(net):
+    for i, entity in enumerate(net.entities):
+        entity.id = "epn_{0}".format(i)
+        for j, subentity in enumerate(entity.components): # should be made recursive
+            subentity.id = "{0}_sub_{1}".format(entity.id, j)
+        for k, sv in enumerate(entity.svs):
+            sv.id = "{0}_sv_{1}".format(entity.id, k)
+        for l, ui in enumerate(entity.uis):
+            ui.id = "{0}_sv_{1}".format(entity.id, l)
+    for i, compartment in enumerate(net.compartments):
+        compartment.id = "comp_{0}".format(i)
+    for i, process in enumerate(net.processes):
+        process.id = "proc_{0}".format(i)
+    for i, op in enumerate(net.logical_operator_nodes):
+        op.id = "op_{0}".format(i)
+
+def write_SBGNML(net, filename):
+    sbgn = libsbgn.sbgn()
+    sbgnmap = libsbgn.map()
+    language = libsbgn.Language.PD
+    sbgnmap.set_language(language)
+    sbgn.set_map(sbgnmap);
+    dids = {}
+    csbgnpy.pd.utils._renew_ids(net)
+    for entity in net.entities:
+        g = csbgnpy.pd.utils._make_glyph_from_entity(entity)
+        sbgnmap.add_glyph(g)
+        dids[entity] = g.get_id()
+    for op in net.logical_operator_nodes:
+        g = utils._make_glyph_from_logical_operaor_node(op)
+        sbgnmap.add_glyph(g)
+        dids[op] = g.get_id()
+        arcs = utils._make_arcs_from_logical_operator_node(op, dids)
+        for arc in arcs:
+            sbgnmap.add_arc(arc)
+    for process in net.processes:
+        p = csbgnpy.pd.utils._make_glyph_from_process(process)
+        sbgnmap.add_glyph(p)
+        dids[process] = p.get_id()
+        arcs = csbgnpy.pd.utils._make_arcs_from_process(process, dids)
+        for arc in arcs:
+            sbgnmap.add_arc(arc)
+    for modulation in net.modulations:
+        arc = csbgnpy.pd.utils._make_arc_from_modulation(modulation, dids)
+        sbgnmap.add_arc(arc)
+    sbgn.write_file(filename)
+    ifile = open(filename)
+    s = ifile.read()
+    ifile.close()
+    s = s.replace("sbgn:","")
+    s = s.replace(' xmlns:sbgn="http://sbgn.org/libsbgn/0.2"', "")
+    s = s.replace('."', '.0"')
+    ofile = open(filename, "w")
+    ofile.write(s)
+    ofile.close()
