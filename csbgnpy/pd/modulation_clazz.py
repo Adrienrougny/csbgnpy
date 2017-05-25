2c1659a788be27269488d172d9a38649783cd115
Adrien Rougny <adrienrougny@gmail.com>
2017-05-25 12:58:25 +0900

    nettoyage

--- csbgnpy/pd/modulation_clazz.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class ModulationClazz(Enum):
-	CATALYSIS  = "catalysis"
-	MODULATION  = "modulation"
-	STIMULATION  = "stimulation"
-	INHIBITION  = "inhibition"
-	UNKNOWN_INFLUENCE  = "unknown influence"
-	NECESSARY_STIMULATION  = "necessary stimulation"
