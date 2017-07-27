98e526ab4576e7fc892176648411ed9d58aca40e
Adrien Rougny <adrienrougny@gmail.com>
2017-07-27 11:44:05 +0900

    af logical operators

---
+++ csbgnpy/af/lo.py
@@ -0,0 +1,27 @@
+# from LogicalOperatorClazz import *
+
+class LogicalOperator(object):
+    def __init__(self, children = None, id = None):
+        self.children = children if children else []
+
+    def add_child(self, child):
+        if child not in self.children:
+            self.children.append(child)
+
+    def __hash__(self):
+        return hash((self.__class__, frozenset(self.children)))
+
+    def __eq__(self, other):
+        return self.__class__ == other.__class__ and \
+        set(self.children) == set(other.children)
+
+class AndOperator(LogicalOperator):
+    pass
+
+class OrOperator(LogicalOperator):
+    pass
+
+class NotOperator(LogicalOperator):
+    pass
+
+
