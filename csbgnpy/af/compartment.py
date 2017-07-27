f2dbd0474cecd4db52f5bbfe710f3134d316a7f8
Adrien Rougny <adrienrougny@gmail.com>
2017-07-27 12:10:20 +0900

    af/compartment.py

--- csbgnpy/af/compartment.py
+++ csbgnpy/af/compartment.py
@@ -1,12 +1,13 @@
-
-
-class Compartment:
-    def __init__(self, id = None, label = None):
-        self.id = id
+class Compartment(object):
+    def __init__(self, label = None, id = None):
         self.label = label
+        self.id = id
 
     def __eq__(self, other):
         return isinstance(other, Compartment) and self.label == other.label
+
+    def __repr__(self):
+        return "Compartment[%s (%s)]" % (self.label, self.id)
 
     def __hash__(self):
         return hash(self.label)
