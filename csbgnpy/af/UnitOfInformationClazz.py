5f098bdf4dac806447679b007735149b8de13b30
Adrien Rougny <adrienrougny@gmail.com>
2017-07-27 12:09:19 +0900

    af/ui.py

--- csbgnpy/af/UnitOfInformationClazz.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class UnitOfInformationClazz(Enum):
-    MACROMOLECULE = {"name": "macromolecule", "h" : 40, "w" : 40}
-    NUCLEIC_ACID_FEATURE = {"name": "nucleic acid feature", "h" : 40, "w" : 40}
-    SIMPLE_CHEMICAL = {"name": "simple chemical", "h" : 40, "w" : 40}
-    UNSPECIFIED_ENTITY = {"name": "unspecified entity", "h" : 40, "w" : 40}
-    COMPLEX = {"name": "complex", "h" : 40, "w" : 40}
-    PERTURBATION = {"name": "perturbation", "h" : 40, "w" : 40}
