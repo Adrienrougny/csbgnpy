1974f18fb4b3da14690adf34e306f2f1fd059d1d
Adrien Rougny <adrienrougny@gmail.com>
2017-08-02 13:59:38 +0900

    pd/entity.py: +get_ui, get_sv

--- csbgnpy/pd/entity.py
+++ csbgnpy/pd/entity.py
@@ -33,6 +33,32 @@ class StatefulEntity(Entity):
         if ui not in self.uis:
             self.uis.append(ui)
 
+    def get_ui(self, val, by_ui = False, by_id = False, by_hash = False):
+        for ui in self.uis:
+            if by_object:
+                if ui == val:
+                    return ui
+            if by_id:
+                if ui.id == val:
+                    return ui
+            if by_hash:
+                if hash(ui) == val:
+                    return ui
+        return None
+
+    def get_sv(self, val, by_sv = False, by_id = False, by_hash = False):
+        for sv in self.svs:
+            if by_object:
+                if sv == val:
+                    return sv
+            if by_id:
+                if sv.id == val:
+                    return sv
+            if by_hash:
+                if hash(sv) == val:
+                    return sv
+        return None
+
     def __eq__(self, other):
         return self.__class__ == other.__class__ and \
             self.label == other.label and \
