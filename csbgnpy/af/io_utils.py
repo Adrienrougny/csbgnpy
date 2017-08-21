121b3525bd512bc58c62d0f0ff7ce1532bcf5fd7
Adrien Rougny <adrienrougny@gmail.com>
2017-08-21 15:30:36 +0900

    af: +io_utils.py

---
+++ csbgnpy/af/io_utils.py
@@ -0,0 +1,26 @@
+from enum import Enum
+import libsbgnpy.libsbgn as libsbgn
+from csbgnpy.utils import *
+from csbgnpy.af.compartment import *
+from csbgnpy.af.activity import *
+from csbgnpy.af.modulation import *
+from csbgnpy.af.lo import *
+from csbgnpy.af.ui import *
+from csbgnpy.af.network import *
+
+class ActivityEnum(Enum):
+    BIOLOGICAL_ACTIVITY = BiologicalActivity
+    PHENOTYPE = Phenotype
+
+class LogicalOperatorEnum(Enum):
+    OR = OrOperator
+    AND = AndOperator
+    NOT = NotOperator
+    DELAY = DelayOPerator
+
+class ModulationEnum(Enum):
+    INFLUENCE  = Modulation
+    POSITIVE_INFLUENCE  = Stimulation
+    NEGATIVE_INFLUENCE  = Inhibition
+    UNKNOWN_INFLUENCE  = Modulation
+    NECESSARY_STIMULATION  = NecessaryStimulation
--- csbgnpy/af/sbgnmlio.py
+++ csbgnpy/af/sbgnmlio.py
@@ -7,22 +7,7 @@ from csbgnpy.af.modulation import *
 from csbgnpy.af.lo import *
 from csbgnpy.af.ui import *
 from csbgnpy.af.network import *
-
-class ActivityEnum(Enum):
-    BIOLOGICAL_ACTIVITY = BiologicalActivity
-    PHENOTYPE = Phenotype
-
-class LogicalOperatorEnum(Enum):
-    OR = OrOperator
-    AND = AndOperator
-    NOT = NotOperator
-
-class ModulationEnum(Enum):
-    INFLUENCE  = Modulation
-    POSITIVE_INFLUENCE  = Stimulation
-    NEGATIVE_INFLUENCE  = Inhibition
-    UNKNOWN_INFLUENCE  = Modulation
-    NECESSARY_STIMULATION  = NecessaryStimulation
+from csbgnpy.af.io_utils import *
 
 def read(*filenames):
     net = Network()
