e24b03c7158043a28700a312e571b8f332b00b5a
Adrien Rougny <adrienrougny@gmail.com>
2018-03-27 07:47:48 +0900

    pd/subentity.py: added doc

--- csbgnpy/pd/subentity.py
+++ csbgnpy/pd/subentity.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 from csbgnpy.pd.sv import UndefinedVar
 
 class SubEntity(object):
+    """The class to model subentities"""
     def __init__(self, id = None):
         self.id = id
 
@@ -43,6 +44,7 @@ class SubEntity(object):
         return str(self)
 
 class StatefulSubEntity(SubEntity):
+    """The class to model stateful subentities"""
     def __init__(self, label = None, svs = None, uis = None, id = None):
         super().__init__(id)
         self.label = label
@@ -66,10 +68,28 @@ class StatefulSubEntity(SubEntity):
             self.svs.append(sv)
 
     def add_ui(self, ui):
+        """Adds a unit of information to the subentity
+
+        :param ui: the unit of information to be added
+        :return: None
+        """
         if ui not in self.uis:
             self.uis.append(ui)
 
     def get_ui(self, val, by_ui = False, by_id = False, by_hash = False):
+        """Retrieves a unit of information from the subentity
+
+        Possible ways of searching for the unit of information are: by object, id, or hash.
+        Only the first matching unit of information is retrieved.
+        Returns None if no matching unit of information is found.
+
+        :param val: the value to be searched
+        :param by_ui: if True, search by object
+        :param by_id: if True, search by id
+        :param by_hash: if True, search by hash
+        :return: the unit of information or None
+        """
+
         for ui in self.uis:
             if by_ui:
                 if ui == val:
@@ -83,6 +103,18 @@ class StatefulSubEntity(SubEntity):
         return None
 
     def get_sv(self, val, by_sv = False, by_id = False, by_hash = False):
+        """Retrieves a state variable from the subentity
+
+        Possible ways of searching for the state variable are: by object, id, or hash.
+        Only the first matching state variable is retrieved.
+        Returns None if no matching state variable is found.
+
+        :param val: the value to be searched
+        :param by_ui: if True, search by object
+        :param by_id: if True, search by id
+        :param by_hash: if True, search by hash
+        :return: the state variable or None
+        """
         for sv in self.svs:
             if by_sv:
                 if sv == val:
@@ -105,6 +137,7 @@ class StatefulSubEntity(SubEntity):
         return hash((self.__class__, self.label, frozenset(self.svs), frozenset(self.uis)))
 
 class StatelessSubEntity(SubEntity):
+    """The class to model stateless subentities"""
     def __init__(self, label = None, id = None):
         super().__init__(id)
         self.label = label
@@ -117,23 +150,33 @@ class StatelessSubEntity(SubEntity):
         return hash((self.__class__, self.label))
 
 class SubUnspecifiedEntity(StatelessSubEntity):
+    """The class to model unspecified subentities"""
     pass
 
 class SubSimpleChemical(StatefulSubEntity):
+    """The class to model simple chemical subentities"""
     pass
 
 class SubMacromolecule(StatefulSubEntity):
+    """The class to model macromolecule subentities"""
     pass
 
 class SubNucleicAcidFeature(StatefulSubEntity):
+    """The class to model nucleic acid feature subentities"""
     pass
 
 class SubComplex(StatefulSubEntity):
+    """The class to model complex subentities"""
     def __init__(self, label = None, svs = None, uis = None, components = None, id = None):
         super().__init__(label, svs, uis, id)
         self.components = components if components is not None else []
 
     def add_component(self, component):
+        """Adds a subentity to the complex subentity
+
+        :param component: the subentity to be added
+        :return: None
+        """
         if component not in self.components:
             self.components.append(component)
 
@@ -148,18 +191,23 @@ class SubComplex(StatefulSubEntity):
         return hash((self.__class__, self.label, frozenset(self.svs), frozenset(self.uis), frozenset(self.components)))
 
 class SubMultimer(StatefulSubEntity):
+    """The class to model multimer subentities"""
     pass
 
 class SubSimpleChemicalMultimer(SubMultimer):
+    """The class to model simple chemical multimer subentities"""
     pass
 
 class SubMacromoleculeMultimer(SubMultimer):
+    """The class to model macromolecule multimer subentities"""
     pass
 
 class SubNucleicAcidFeatureMultimer(SubMultimer):
+    """The class to model nucleic acid feature multimer subentities"""
     pass
 
 class SubComplexMultimer(SubComplex, SubMultimer):
+    """The class to model nucleic complex multimer subentities"""
     def __init__(self, label = None, svs = None, uis = None, components = None, id = None):
         super().__init__(label, svs, uis, component, id)
 
