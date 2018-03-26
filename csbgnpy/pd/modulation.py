d32dd5006fbd4cb84ac780fb9ce56649d26d2cf8
Adrien Rougny <adrienrougny@gmail.com>
2018-03-27 07:26:30 +0900

    pd/modulations.py: added absolute modulation + doc

--- csbgnpy/pd/modulation.py
+++ csbgnpy/pd/modulation.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 
 class Modulation(object):
+    """The class to model modulations"""
     def __init__(self, source = None, target = None, id = None):
         self.source = source
         self.target = target
@@ -29,18 +30,25 @@ class Modulation(object):
         return "{}({},{})".format(self.__class__.__name__, self.source, self.target)
 
 class Stimulation(Modulation):
+    """The class to model stimulations"""
     pass
 
 class Inhibition(Modulation):
+    """The class to model inhibitions"""
     pass
 
 class Catalysis(Stimulation):
+    """The class to model stimulations"""
     pass
 
 class NecessaryStimulation(Stimulation):
+    """The class to model necessary stimulations"""
     pass
 
 class AbsoluteInhibition(Inhibition):
+    """The class to model absolute inhibitions"""
     pass
 
-
+class AbsoluteStimulation(Stimulation):
+    """The class to model absolute stimulations"""
+    pass
